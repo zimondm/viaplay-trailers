19fb7528e59c5459c6329127db684389e65c141d
Zimon Mossberg <zimon.mossberg@attentec.se>
2020-07-14 12:33:41 +0200

    feat: added simple rate limiter example

--- api/trailer/trailer.routes.js
+++ api/trailer/trailer.routes.js
@@ -1,15 +1,20 @@
 const router = require('express').Router();
 const { getTrailer } = require('./trailer.controller');
 const { cacheGet, cacheSet } = require('../cache/cache');
-
+const rateLimit = require('express-rate-limit');
+const trailerLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+});
 /*
     The trailer endpoint.
 
     Query parameters:
     * movieURI - a viaplay content uri
 
-    in memory cache enabled for this endpoint with a ttl of 5 minutes.
+    In memory cache enabled for this endpoint with a ttl of 5 minutes.
+    Example rate limiting applied with a max of 100 requests/min per ip
 */
-router.get('/trailer', cacheSet(300), cacheGet, getTrailer);
+router.get('/trailer', trailerLimiter, cacheSet(300), cacheGet, getTrailer);
 
 module.exports = router;
